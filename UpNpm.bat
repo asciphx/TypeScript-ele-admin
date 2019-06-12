6931884fc6488eb126ab029e1172f6be80d69523
asciphx <maxspore@foxmail.com>
2019-06-12 14:52:17 +0800

    Best in the world<maxspore@foxmail.com>

--- UpNpm.bat
+++ UpNpm.bat
@@ -1,3 +1,3 @@
-@echo off  & for /f "delims=^" %%i in ('npm outdated --parseable --depth=0') do (
+@echo off  & (for /f "delims=^" %%i in ('npm outdated --parseable --depth=0') do (
 for /f "delims=:" %%i in ("%%~ni") do (npm install %%i@latest)
-)& pause
+))& pause
